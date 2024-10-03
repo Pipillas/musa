5995049e09b920d3f5e3f948f7727550ebfb7ac5
PipasMusa <masdamianr@gmail.com>
2024-10-03 20:11:10 -0300

    cambios

--- musa_frontend/src/main.jsx
+++ musa_frontend/src/main.jsx
@@ -1,13 +1,32 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
 import io from 'socket.io-client';
 import './styles.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
-export const IP = `https://musavinos.com`
+let ip_variable = `https://musavinos.com`; // Valor inicial de la IP
 
-export const socket = io(`${IP}`);
+export const IP = () => ip_variable; // Exporta la función IP que devuelve el valor actual
 
-createRoot(document.getElementById('root')).render(
-  <App />
-);
+export let socket;
+
+// Función para verificar la conexión al servidor remoto
+const checkConnection = async () => {
+  try {
+    const response = await fetch(ip_variable, { method: 'HEAD', mode: 'no-cors' });
+    if (response.ok || response.type === 'opaque') {
+      console.log('Conexión exitosa a', ip_variable);
+    } else {
+      throw new Error('Fallo en la conexión remota');
+    }
+  } catch (error) {
+    console.log('No se pudo conectar a', ip_variable, ', usando 192.168.1.39.');
+    ip_variable = 'http://192.168.1.39:5000'; // Cambia ip_variable a localhost si falla la conexión
+  }
+
+  socket = io(ip_variable); // Inicializa el socket con la IP actual
+  createRoot(document.getElementById('root')).render(<App />);
+};
+
+// Llama a la función para verificar y conectar
+checkConnection();
